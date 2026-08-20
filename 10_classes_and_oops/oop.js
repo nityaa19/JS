315129aec4be004ee6d64c22ad0ecda1e82a32ce
Nityanand <nityanand1909@gmail.com>
2026-08-20 23:48:01 +0530

    OOPS in JS

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -1,7 +1,42 @@
-const user ={
-  username: "hitesh",
+const user =  {
+  username: "Nityanand",
   loginCount: 8,
-  signedIn: true
+  signedIn: true,
+
+  getUserDetails: function(){
+    // console.log(`username: ${this.username}`);
+    console.log(this);
+    
+    
+  }
 }
 
-console.log(user.username)
+// console.log(user.username);
+// console.log(user.getUserDetails())
+// console.log(this);// -> {}
+
+
+
+function User(username, loginCount, isLoggedIn){
+  this.username= username
+  this.loginCount=loginCount
+  this.isLoggedIn=isLoggedIn
+
+  this.greeting=function(){
+    console.log(`Welcome ${this.username}`);
+  }
+
+  return this
+}
+
+const userOne= new User("hitesh",9,true)
+/*
+by using new keyword new object is created everytime in step 1
+step 2: constructor function is called (pack all the arguments and give)
+step 3: all the arguments are injected into this keyword
+step 4: return in the function 
+*/
+const userTwo= new User("Nitya",10,false)
+console.log(userOne.constructor)
+// console.log(userTwo)
+
